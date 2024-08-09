b7a61644708ae281b48dd64d29b4035ac96f4803
scott christian mccallum <scottcmccallum@gmail.com>
2024-08-09 17:46:39 -0400

    add local basemap info and locations

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -2,27 +2,109 @@
     import { onMount } from "svelte";
     import maplibregl from "maplibre-gl";
     import "maplibre-gl/dist/maplibre-gl.css";
-  
+    import * as pmtiles from "pmtiles";
+    import BaseLayer from "/src/data/toronto.json";
+
     let map;
-  
-    onMount(() => {
-      map = new maplibregl.Map({
-        container: "map", // The id of the div where the map will be displayed
-        style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json", // Map style URL
-        center: [-79.38919989374392, 43.641655778152966], // Set to UofT or desired center
-        zoom: 12 // Initial zoom level
-      });
-  
-      // Add any additional map layers, controls, or interactions here
+    let scale = new maplibregl.ScaleControl({
+        maxWidth: 100,
+        unit: "metric",
     });
-  </script>
-  
-  <style>
+    let PMTILES_URL = "/src/data/toronto.pmtiles";
+
+    const maxBounds = [
+        [-79.7712, 43.44], // SW coords
+        [-78.914763, 43.93074], // NE coords
+    ];
+
+    onMount(async () => {
+        let protocol = new pmtiles.Protocol();
+        maplibregl.addProtocol("pmtiles", protocol.tile);
+
+        map = new maplibregl.Map({
+            container: "map",
+            style: {
+                version: 8,
+                name: "Empty",
+                glyphs: "https://schoolofcities.github.io/fonts/fonts/{fontstack}/{range}.pbf",
+                sources: {},
+                layers: [
+                    {
+                        id: "background",
+                        type: "background",
+                        paint: {
+                            "background-color": "rgba(0,0,0,0)",
+                        },
+                    },
+                ],
+            },
+            center: [-79.405, 43.647],
+            zoom: 9.4,
+            maxZoom: 16.5,
+            minZoom: 9,
+            maxPitch: 85,
+            bearing: -17.1,
+            projection: "globe",
+            scrollZoom: true,
+            maxBounds: maxBounds,
+            attributionControl: true,
+        });
+
+        const attributions = [
+            '<a href="https://openstreetmap.org">OpenStreetMap</a>',
+            '<a href="https://open.toronto.ca/">City of Toronto </a>',
+        ];
+        const attributionString = attributions.join(", ");
+        let protoLayers = BaseLayer;
+
+        map.addControl(scale, "bottom-left");
+        map.addControl(new maplibregl.NavigationControl(), "top-left");
+        map.dragRotate.disable();
+
+        const response = await fetch("/src/data/campus_locations.geojson");
+        const geojson = await response.json();
+
+        map.on("load", function () {
+            map.addSource("protomaps", {
+                type: "vector",
+                url: "pmtiles://" + PMTILES_URL,
+                attribution: attributionString,
+                attributionControl: false,
+            });
+
+            protoLayers.forEach((e) => {
+                map.addLayer(e);
+            });
+
+            map.addSource("campus-locations", {
+                type: "geojson",
+                data: geojson,
+            });
+
+            map.addLayer({
+                id: "campus-points",
+                type: "circle",
+                source: "campus-locations",
+                paint: {
+                    "circle-radius": 8,
+                    "circle-color": "#007cbf",
+                },
+            });
+        });
+
+        // Console log zoom and center
+        map.on("moveend", () => {
+            console.log("Map center:", map.getCenter());
+            console.log("Map zoom level:", map.getZoom());
+        });
+    });
+</script>
+
+<div id="map"></div>
+
+<style>
     #map {
-      width: 720px;
-      height: 50vh;
+        width: 720px;
+        height: 50vh;
     }
-  </style>
-  
-  <div id="map"></div>
-  
+</style>
